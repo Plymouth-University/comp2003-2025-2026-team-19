ab224b90aaf2a1cb4daeebe103c84a99a032137f
isaa-ctaylor <isaac@isaactaylor.xyz>
2025-11-21 22:59:59 +0000

    feat: bare API server with Sentry integration

---
+++ web/api/src/main.py
@@ -0,0 +1,25 @@
+import os
+
+import sentry_sdk
+from dotenv import load_dotenv
+from fastapi import FastAPI
+
+load_dotenv()
+
+sentry_sdk.init(
+    dsn=os.getenv("SENTRY_DSN"),
+    send_default_pii=True,
+    # Enable sending logs to Sentry
+    enable_logs=True,
+    # Set traces_sample_rate to 1.0 to capture 100%
+    # of transactions for tracing.
+    traces_sample_rate=1.0,
+    # Set profile_session_sample_rate to 1.0 to profile 100%
+    # of profile sessions.
+    profile_session_sample_rate=1.0,
+    # Set profile_lifecycle to "trace" to automatically
+    # run the profiler on when there is an active transaction
+    profile_lifecycle="trace",
+)
+
+app = FastAPI()
