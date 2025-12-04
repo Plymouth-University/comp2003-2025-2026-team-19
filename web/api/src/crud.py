dfdacd46a7201ba296f30dd72384a2f06f4eeba6
isaa-ctaylor <isaac@isaactaylor.xyz>
2025-12-04 00:41:39 +0000

    feat: add crud function for reading only active entities

--- web/api/src/crud.py
+++ web/api/src/crud.py
@@ -2,17 +2,34 @@ import uuid
 
 from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy.orm import selectinload
 
-from .models import Entity
+from .models import Attribute, Entity
 
 
 async def read_entity_by_id(db: AsyncSession, entity_id: uuid.UUID) -> Entity | None:
     """Read an entity by its ID."""
-    result = await db.execute(select(Entity).where(Entity.id == entity_id))
-    return result.scalars().first()
+    result = await db.execute(
+        select(Entity)
+        .where(Entity.id == entity_id)
+        .join(Entity.attributes)
+        .options(selectinload(Entity.attributes))
+    )
+    entity = result.scalars().first()
+
+    if not entity:
+        return None
+
+    return entity
 
 
 async def read_all_entities(db: AsyncSession) -> list[Entity]:
     """Read all entities."""
     result = await db.execute(select(Entity))
+    return result.scalars().all()  # type: ignore
+
+
+async def read_all_active_entities(db: AsyncSession) -> list[Entity]:
+    """Read all active entities."""
+    result = await db.execute(select(Entity).where(Entity.active == True))
     return result.scalars().all()  # type: ignore
