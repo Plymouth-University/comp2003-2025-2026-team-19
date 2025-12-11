d5661197a320aa114a094dd5ae6a0f48aa4f2cd9
CoolMann2 <alexander.nemeth@students.plymouth.ac.uk>
2025-12-11 19:37:55 +0000

    Adjusted javascript entitylist logic to new json data, based on if a route is null or not

--- web/frontend/static/entity_list.js
+++ web/frontend/static/entity_list.js
@@ -1,34 +1,44 @@
 const entity_list = [
-    {
-        "entity_id": "e22f0bc9-f01f-4d7d-a37d-748af20165d4",
-        "journey": {
-            "name": "Cremyll to Royal William Yard"
-        }
-    },
-    {
-        "entity_id": "a91c3b1e-2c4a-4e8f-9a0d-5f3e2d1b0c9f",
-        "journey": {
-            "name": "Plymouth Hoe to Mount Batten"
-        }
-    },
-    {
-        "entity_id": "b3e8d7a2-7f6c-4b5d-8c1a-9e0f3d2c1b4a",
-        "journey": {
-            "name": "Torpoint to Devonport"
-        }
-    },
-    {
-        "entity_id": "c6f2a4d9-1b3e-4c5f-9d7b-8a9c0e1d2f3g",
-        "journey": {
-            "name": "Saltash to Brunel Green"
-        }
-    },
-    {
-        "entity_id": "d1g5h8i3-j2k4-l6m8-n0p9-q7r5s3t1u0v9",
-        "journey": {
-            "name": "Bovisand to Cawsand"
-        }
+  {
+    "id": "b85e5637-4791-4aa4-abec-a2ac3e4a946e",
+    "name": "Plymouth Venturer",
+    "route": null
+  },
+  {
+    "id": "fa7fa868-8eac-4e20-a69c-b7b7c17364bb",
+    "name": "Plymouth Sound",
+    "route": null
+  },
+  {
+    "id": "fa0a1599-8de1-4e08-aee8-f1607e1359cb",
+    "name": "Tamar Belle",
+    "route": null
+  },
+  {
+    "id": "add9ce40-e60c-41b4-b6e9-e7dc3185849c",
+    "name": "Plymouth Princess",
+    "route": null
+  },
+  {
+    "id": "9e2f2cbd-2671-4972-9115-c0bd8d5cef2d",
+    "name": "Island Princess",
+    "route": null
+  },
+  {
+    "id": "072caf2a-a921-4128-8369-75b237c42161",
+    "name": "Edgcumbe Belle",
+    "route": {
+      "id": "b7ce8114-7ca1-4a6e-8e03-37b261613d87",
+      "start_location": {
+        "id": "21817854-9c58-445d-bbfe-be9de592a214",
+        "name": "Plymouth Stonehouse Ferry Terminal"
+      },
+      "end_location": {
+        "id": "2300f6f1-6fd9-49a5-8cab-cc4415d02dc7",
+        "name": "Cremyll Mount Edgcumbe Ferry Landing"
+      }
     }
+  }
 ]
 
 function updateEntityList(entity_list) {
@@ -41,14 +51,28 @@ function updateEntityList(entity_list) {
     //Goes through entities in the json
     entity_list.forEach(entity => {
       const li = document.createElement("li");
+      
+      //List acts differently depending if route data is null or not
+      if (entity.route === null) {
       //Uses innerHTML to display entities and use break
       li.innerHTML = `
-      <a href="/status/${entity.entity_id}">
-      ${entity.journey.name}
-      </a><br>
+      <a href="/status/${entity.id}">
+      ${entity.name}
+      </a>
     `;
+    } else {
+        li.innerHTML = `
+        <a href="/status/${entity.id}">
+           ${entity.name}:<br>
+           ${entity.route.start_location.name}
+           →
+           ${entity.route.end_location.name}
+           </a>
+        `;
+    }
     //Adds list to container
     entity_container.appendChild(li);
+    entity_container.appendChild(document.createElement("br"));
   });
 }
 
