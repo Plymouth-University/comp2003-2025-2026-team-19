49e789c84cfd1ab67efa34aa9c32e0f49c2ace0c
isaa-ctaylor <isaac@isaactaylor.xyz>
2026-02-24 23:11:47 +0000

    feat(api, ingestion, frontend): unify health checks

--- ingestion/server/src/main.py
+++ ingestion/server/src/main.py
@@ -1,6 +1,8 @@
 from fastapi import FastAPI
 
 from .routes.entity import router as entity_router
+from .routes.health import router as health_router
 
 app = FastAPI(title="ingestion", root_path="/api/v1")
 app.include_router(entity_router)
+app.include_router(health_router)
