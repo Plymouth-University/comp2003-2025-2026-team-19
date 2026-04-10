240dfd51cf01cd23a1ffc6276f41c8a9e2321601
isaa-ctaylor <isaac@isaactaylor.xyz>
2026-04-10 20:41:29 +0100

    refactor: typehint create_test_entity

--- tests/utils.py
+++ tests/utils.py
@@ -1,7 +1,9 @@
+from sqlalchemy.orm import Session
+
 from core.models import Entity
 
 
-def create_test_entity(db_session, name="Test Entity"):
+def create_test_entity(db_session: Session, name="Test Entity"):
     entity = Entity(name=name)
     db_session.add(entity)
     db_session.commit()
