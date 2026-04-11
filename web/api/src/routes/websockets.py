a66aab487af04f9bbf4b292a0894611ca7ec5576
isaa-ctaylor <isaac@isaactaylor.xyz>
2026-04-11 22:46:20 +0100

    feat(websocket): implement schema checking and appropriate disconnect logic

--- web/api/src/routes/websockets.py
+++ web/api/src/routes/websockets.py
@@ -1,12 +1,16 @@
 import asyncio
 import json
 import logging
+from typing import Annotated, Literal, Union
+from uuid import UUID
 
 import redis.asyncio as redis
 from fastapi import APIRouter, Depends, WebSocket, WebSocketDisconnect
 from geoalchemy2.shape import to_shape
+from pydantic import BaseModel, BeforeValidator, Field, ValidationError
 
 from core.database import AsyncSession, get_db_session
+from core.helpers import filter_uuids
 from core.settings import settings
 
 from .. import crud
@@ -43,6 +47,21 @@ class TrackingWebSocketManager:
 ws_manager = TrackingWebSocketManager()
 
 
+SafeUUIDList = Annotated[list[UUID], BeforeValidator(filter_uuids)]
+
+
+class SubscribeAction(BaseModel):
+    action: Literal["subscribe"]
+    entity_ids: SafeUUIDList | Literal["all"] | None = None
+
+
+WSAction = Union[SubscribeAction]
+
+
+class WSRequest(BaseModel):
+    data: WSAction = Field(..., discriminator="action")
+
+
 @router.websocket("/entities/ws")
 async def entities_websocket(
     websocket: WebSocket, db: AsyncSession = Depends(get_db_session)
@@ -50,13 +69,37 @@ async def entities_websocket(
     await ws_manager.connect(websocket)
     try:
         while True:
-            message: dict = await websocket.receive_json()
-            if message.get("action") == "subscribe":
-                entity_info = await crud.get_entities_info(
-                    db, message.get("entity_ids", [])
-                )
+            try:
+                raw_data: dict = await websocket.receive_json()
+                request = WSRequest(data=raw_data)  # type: ignore
+                data = request.data
+            except json.JSONDecodeError:
+                await websocket.close(code=1007, reason="Invalid JSON")
+                ws_manager.disconnect(websocket)
+                break
+            except ValidationError as e:
+                # Catch missing fields or invalid action names
+                errors = e.errors()
+                if errors[0]["type"] == "union_tag_invalid":
+                    await websocket.close(code=1008, reason="Invalid action")
+                elif errors[0]["type"] == "union_tag_not_found":
+                    await websocket.close(code=1007, reason="Missing action")
+                else:
+                    await websocket.close(code=1008, reason=str(errors))
+                ws_manager.disconnect(websocket)
+                break
+            if data.action == "subscribe":
+                if data.entity_ids is not None:
+                    entity_ids = (
+                        [e for e in data.entity_ids if type(e) == UUID]
+                        if type(data.entity_ids) == list
+                        else data.entity_ids
+                    )
+                    entity_info = await crud.get_entities_info(db, entity_ids)
 
-                await ws_manager.subscribe(websocket, list(entity_info.keys()))
+                    await ws_manager.subscribe(websocket, list(entity_info.keys()))
+                else:
+                    entity_info = {}
 
                 await websocket.send_json(
                     {
@@ -64,6 +107,10 @@ async def entities_websocket(
                         "entities": entity_info,
                     }
                 )
+            else:
+                await websocket.close(code=1008, reason="Invalid action")
+                ws_manager.disconnect(websocket)
+                break
     except WebSocketDisconnect:
         ws_manager.disconnect(websocket)
     except Exception as e:
