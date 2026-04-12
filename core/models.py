b351106ce769a1e31de119cc1a962ccc3788abbc
CoolMann2 <alexander.nemeth@students.plymouth.ac.uk>
2026-04-12 20:44:09 +0100

    Reverted models.py

--- core/models.py
+++ core/models.py
@@ -1,5 +1,5 @@
 import datetime
-import uuid as uuid_lib
+import uuid
 
 from geoalchemy2 import Geometry, WKBElement
 from sqlalchemy import (
@@ -28,9 +28,9 @@ class Entity(Base):
     __tablename__ = "Entity"
 
     id: Mapped[int] = mapped_column(Integer, primary_key=True, autoincrement=True)
-    uuid: Mapped[uuid_lib.UUID] = mapped_column(
+    uuid: Mapped[uuid.UUID] = mapped_column(
         UUID(as_uuid=True),
-        default=uuid_lib.uuid4,
+        default=uuid.uuid4,
         unique=True,
         nullable=False,
         server_default=func.gen_random_uuid(),
@@ -107,9 +107,9 @@ class Route(Base):
     __tablename__ = "Route"
 
     id: Mapped[int] = mapped_column(Integer, primary_key=True, autoincrement=True)
-    uuid: Mapped[uuid_lib.UUID] = mapped_column(
+    uuid: Mapped[uuid.UUID] = mapped_column(
         UUID(as_uuid=True),
-        default=uuid_lib.uuid4,
+        default=uuid.uuid4,
         unique=True,
         nullable=False,
         server_default=func.gen_random_uuid(),
